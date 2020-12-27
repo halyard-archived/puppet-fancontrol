9a8dd93563336a5199605099779cc0f96dd7d91b
Les Aker <me@lesaker.org>
2020-12-27 10:41:46 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+puppet-fancontrol
+===========
+
+[![Puppet Forge](https://img.shields.io/puppetforge/v/halyard/fancontrol.svg)](https://forge.puppetlabs.com/halyard/fancontrol)
+[![MIT Licensed](http://img.shields.io/badge/license-MIT-green.svg?style=flat)](https://tldrlegal.com/license/mit-license)
+
+Module to configure fancontrol service for servers
+
+## Usage
+
+```puppet
+include fancontrol
+```
+
---
+++ manifests/init.pp
@@ -0,0 +1,26 @@
+# == Class: hostname
+#
+# Set up fancontrol service
+#
+# === Parameters
+#
+# === Example
+#
+#   class { 'fancontrol': }
+#
+
+class fancontrol (
+  Array[String] $modules = ['coretemp', 'jc42', 'w83627ehf', 'w83795']
+) {
+  package { 'lm_sensors': }
+
+  -> file { '/etc/conf.d/lm_sensors':
+    ensure  => file,
+    content => template('fancontrol/lm_sensors.conf.erb')
+  }
+
+  -> service { 'lm_sensors':
+    ensure  => running,
+    enabled => true
+  }
+}
